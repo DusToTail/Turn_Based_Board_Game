0c99c99124c7e9506c08c1555888668cb77016f7
DustoTail <dustotail@gmail.com>
2022-05-28 23:19:13 +0900

    Added scriptable object for weapon

--- Board Game/Assets/Scripts/Player/Block/Weapon.cs
+++ Board Game/Assets/Scripts/Player/Block/Weapon.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
 {
+    public WeaponScriptableObject weaponScriptableObject;
+
+    public string weaponName;
+    public string weaponDescription;
+
     public int attackDamage;
     public int attackRange;
     public int attackCooldown;
+
+    protected int[,] _attackGrid;
+    protected int _attackGridWidth;
+    protected int _attackGridLength;
 
     protected int _curAttackCooldown;
     protected bool _canAttack;
 
 
-    public abstract void Attack(CharacterBlock[] blocks);
+    public abstract void Attack(CharacterBlock userBlock);
     public void ReduceAttackCooldown(int amount) 
     { 
         if(amount < 0) { return; }
@@ -21,4 +30,21 @@ public abstract class Weapon : MonoBehaviour
     }
     public void ResetAttackCooldown() { _curAttackCooldown = attackCooldown; }
     public void SetCanAttack(bool canAttack) { _canAttack = canAttack; }
+
+    public void InitializeAttackGrid(int[] oneDimensionalGrid, int width, int length)
+    {
+        _attackGridWidth = width;
+        _attackGridLength = length;
+        _attackGrid = new int[length, width];
+        int count = 0;
+        for(int i = 0; i < length; i++)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                if(count > oneDimensionalGrid.Length - 1) { break; }
+                _attackGrid[i,j] = oneDimensionalGrid[count];
+                count++;
+            }
+        }
+    }
 }
---
+++ Board Game/Assets/Scripts/ScriptableObjects/WeaponScriptableObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// English: Scriptable object for weapons in general
+/// </summary>
+[CreateAssetMenu(fileName = "WeaponData", menuName = "Weapon")]
+public class WeaponScriptableObject : ScriptableObject
+{
+    public string weaponName;
+    [TextArea(5, 20)]
+    public string weaponDescription;
+
+    public int attackDamage;
+    public int attackRange;
+    public int attackCooldown;
+
+    public int[] attackGrid;
+    public int attackGridWidth;
+    public int attackGridLength;
+}
