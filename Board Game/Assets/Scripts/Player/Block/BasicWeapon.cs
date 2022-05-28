2e5eac51549bdb36bb7e032fa37136863b6bb709
DustoTail <dustotail@gmail.com>
2022-05-29 01:04:44 +0900

    Modified Attack implementation in BasicWeapon

--- Board Game/Assets/Scripts/Player/Block/BasicWeapon.cs
+++ Board Game/Assets/Scripts/Player/Block/BasicWeapon.cs
@@ -10,21 +10,19 @@ public class BasicWeapon : Weapon
         Cell userCell = userBlock.cell;
         GridDirection userDirection = userBlock.forwardDirection;
         Cell[] attackCells = userBlock.gameManager.gridController.GetCellsFromCellWithDirectionAnd2DGrid(userBlock.cell, userBlock.forwardDirection, _attackGrid);
-        for (int l = 0; l < _attackGridLength; l++)
+        for(int i = 0; i < attackCells.Length; i++)
         {
-            for(int w = 0; w < _attackGridWidth; w++)
-            {
-                if(_attackGrid[l,w] == 0) { continue; }
+            Cell attackCell = attackCells[i];
+            GameObject toAttackBlock = userBlock.gameManager.characterPlane.grid[attackCell.gridPosition.y, attackCell.gridPosition.z, attackCell.gridPosition.x].block;
 
-                GameObject toAttackBlock= userBlock.gameManager.characterPlane.grid[userCell.gridPosition.y, l, w].block;
-                toAttackBlock.GetComponent<CharacterBlock>().TakeDamage(attackDamage);
-                _curAttackCooldown--;
 
-                // Animation and Sound effect
+            // Animation and Sound effect
 
-                // Trigger the occupants' TriggerHit method.
-            }
+            // Trigger the occupants' TriggerHit method.
+            toAttackBlock.GetComponent<CharacterBlock>().TakeDamage(attackDamage);
+            _curAttackCooldown--;
         }
+
     }
 
     
