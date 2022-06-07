d349e190e9c17cf613765666bd0ba0305a41233f
DustoTail <dustotail@gmail.com>
2022-06-08 01:40:10 +0900

    add transition functionality in audio handler

--- Board Game/Assets/Scripts/Player/GameSystem/Audio/AudioHandler.cs
+++ Board Game/Assets/Scripts/Player/GameSystem/Audio/AudioHandler.cs
@@ -26,59 +26,100 @@ public class AudioHandler : MonoBehaviour
 
     public void Play(string audioName)
     {
-        for(int i = 0; i < datas.Length; i++)
-        {
-            if(datas[i].audioName == audioName && datas[i].clip != null)
-            {
-                Debug.Log($"Play audio called {audioName}");
-                sources[i].Play();
-            }
-        }
-        Debug.Log($"There is no audio called {audioName}");
+        AudioSource source = GetSourceFromName(audioName);
+        if (source == null) { return; }
+        Debug.Log($"Play audio called {audioName}"); 
+        source.Play();
     }
 
     public void Stop(string audioName)
     {
-        for (int i = 0; i < datas.Length; i++)
-        {
-            if (datas[i].audioName == audioName && datas[i].clip != null)
-            {
-                Debug.Log($"Play audio called {audioName}");
-                sources[i].Stop();
-            }
-        }
-        Debug.Log($"There is no audio called {audioName}");
+        AudioSource source = GetSourceFromName(audioName);
+        if (source == null) { return; }
+        Debug.Log($"Stop audio called {audioName}"); 
+        source.Stop();
     }
 
     public void Pause(string audioName)
     {
-        for (int i = 0; i < datas.Length; i++)
-        {
-            if (datas[i].audioName == audioName && datas[i].clip != null)
-            {
-                Debug.Log($"Play audio called {audioName}");
-                sources[i].Pause();
-            }
-        }
-        Debug.Log($"There is no audio called {audioName}");
+        AudioSource source = GetSourceFromName(audioName);
+        if (source == null) { return; }
+        Debug.Log($"Pause audio called {audioName}"); 
+        source.Pause();
     }
 
     public void UnPause(string audioName)
     {
+        AudioSource source = GetSourceFromName(audioName);
+        if(source == null) { return; }
+        Debug.Log($"UnPause audio called {audioName}");
+        source.UnPause();
+    }
+
+    public void TransitionFromToAudioSource(string from, string to, float transitionTimeInSeconds)
+    {
+        Debug.Log($"Start transition from audio {from} to {to} in {transitionTimeInSeconds}");
+        StartCoroutine(TransitionFromToAudioSourceCoroutine(from, to, transitionTimeInSeconds));
+    }
+
+    private IEnumerator TransitionFromToAudioSourceCoroutine(string from, string to, float transitionTimeInSeconds)
+    {
+        AudioSource fromSource = GetSourceFromName(from);
+        float differenceFromSourceVolumn = 0 - fromSource.volume;
+        AudioSource toSource = GetSourceFromName(to);
+        float differenceToSourceVolumn = GetDataFromName(to).volume - toSource.volume;
+        float t = 0;
+        while(t <= transitionTimeInSeconds)
+        {
+            yield return null;
+            if(differenceFromSourceVolumn < 0)
+                fromSource.volume += (differenceFromSourceVolumn / transitionTimeInSeconds) * Time.deltaTime;
+            if(differenceToSourceVolumn > 0)
+                toSource.volume += (differenceToSourceVolumn / transitionTimeInSeconds) * Time.deltaTime;
+            t += Time.deltaTime;
+        }
+    }
+
+    public void ResetAudioSourceSetting(string audioName)
+    {
         for (int i = 0; i < datas.Length; i++)
         {
             if (datas[i].audioName == audioName && datas[i].clip != null)
             {
-                Debug.Log($"Play audio called {audioName}");
-                sources[i].UnPause();
+                Debug.Log($"Reset audio called {audioName}");
+                sources[i].clip = datas[i].clip;
+                sources[i].volume = datas[i].volume;
+                sources[i].pitch = datas[i].pitch;
+                sources[i].loop = datas[i].isLoop;
             }
         }
         Debug.Log($"There is no audio called {audioName}");
     }
 
-    public void TransitionFromToAudioSource(AudioSource from, AudioSource to, float t)
+    private AudioData GetDataFromName(string audioName)
     {
+        for (int i = 0; i < datas.Length; i++)
+        {
+            if (datas[i].audioName == audioName && datas[i].clip != null)
+            {
+                return datas[i];
+            }
+        }
+        Debug.Log($"There is no audio called {audioName}");
+        return null;
+    }
 
+    private AudioSource GetSourceFromName(string audioName)
+    {
+        for (int i = 0; i < datas.Length; i++)
+        {
+            if (datas[i].audioName == audioName && datas[i].clip != null)
+            {
+                return sources[i];
+            }
+        }
+        Debug.Log($"There is no audio called {audioName}");
+        return null;
     }
 
 
