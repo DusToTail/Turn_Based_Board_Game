651cffc43e9bfeaa0ddbee0d0ebfc23b00316b99
DustoTail <dustotail@gmail.com>
2022-06-14 23:00:40 +0900

    Added Moves UI

--- Board Game/Assets/Scripts/Player/Systems/UI/MovesUI.cs
+++ Board Game/Assets/Scripts/Player/Systems/UI/MovesUI.cs
@@ -2,17 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages move icons and add/remove it when tracking character's move (action point) increase/decrease
+/// </summary>
 public class MovesUI : MonoBehaviour
 {
+    public GameObject prefabIcon;
+    public CharacterBlock trackingCharacter;
+    public GameManager gameManager;
+
+    [SerializeField]
+    private float distanceInPixels;
+    private int currentIcons;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        GameManager.OnLevelStarted += InitializeHealthUI;
+        GameManager.OnBlockEndedBehaviour += DecrementMove;
+        GameManager.OnPlayerTurnStarted += ResetMove;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnLevelStarted -= InitializeHealthUI;
+        GameManager.OnBlockEndedBehaviour -= DecrementMove;
+        GameManager.OnPlayerTurnStarted -= ResetMove;
+    }
+
+    private void DecrementMove(Block trackingBlock)
+    {
+        if (trackingBlock != trackingCharacter) { return; }
+        if (currentIcons == 0) { return; }
+        transform.GetChild(currentIcons - 1).GetComponent<MoveIcon>().OnRemoved();
+        currentIcons--;
+    }
+
+    private void ResetMove()
+    {
+        currentIcons = transform.childCount;
+        for(int i = 0; i < currentIcons; i++)
+            transform.GetChild(i).GetComponent<MoveIcon>().OnAdded();
+    }
+
+    private void InitializeHealthUI()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+        currentIcons = 0;
+        trackingCharacter = gameManager.playerController.playerBlock;
+        for (int i = 0; i < trackingCharacter.curMovesLeft; i++)
+        {
+            GameObject icon = Instantiate(prefabIcon);
+            RectTransform rectTransform = icon.transform as RectTransform;
+            rectTransform.SetParent(transform);
+            rectTransform.anchoredPosition3D = Vector3.zero + Vector3.left * distanceInPixels * i;
+            currentIcons++;
+        }
     }
 }
