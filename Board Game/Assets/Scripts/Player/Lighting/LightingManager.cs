f40b9a4ddb431518a4d6aae6373e920ff8d9f5b4
DustoTail <dustotail@gmail.com>
2022-06-15 15:45:39 +0900

    Added additional lighting

--- Board Game/Assets/Scripts/Player/Lighting/LightingManager.cs
+++ Board Game/Assets/Scripts/Player/Lighting/LightingManager.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class LightingManager : MonoBehaviour
 {
     public Light playerLight;
+    public Light surroundingLight;
     public Transform playerBlockTransform;
 
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 playerLightOffset;
+    [SerializeField] private Vector3 surroundingLightOffset;
+
 
     private void LateUpdate()
     {
         if(playerLight == null) { return; }
         if(playerBlockTransform == null) { return; }
 
-        playerLight.transform.position = playerBlockTransform.position + offset;
+        playerLight.transform.position = playerBlockTransform.position + playerLightOffset;
+        surroundingLight.transform.position = playerBlockTransform.position + surroundingLightOffset;
     }
 }
